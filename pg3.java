0e9c97a56d633e98c4453a35f12d51bcea0bff7c
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-05 23:17:19 +0530

    3 file commit

---
+++ pg3.java
@@ -0,0 +1,13 @@
+//Escape Sequence Charector
+
+class Test{
+   public static void main(String[] args){
+    System.out.print("A\n");  // \n use for new line 
+    System.out.print("B\n");
+
+    System.out.print("\"welcome\"");
+
+     System.out.println("ABCD\bEFG\rHI");
+     System.out.println("ABCD\bEFG\tHI");
+   }
+}
