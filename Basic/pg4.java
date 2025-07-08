97cc78f0d9ed1e6111aa4873795a89ba41ff25c8
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-08 09:46:53 +0530

    first update

---
+++ Basic/pg4.java
@@ -0,0 +1,34 @@
+//valid signature of main method 
+//final
+//synchronized
+//strictfp
+
+
+
+
+// class Test{
+//   public static void main(String args[]){
+//     System.out.println("Hello India");
+//   }
+// }
+
+// class Test{
+//   public static final void main(String args[]){
+//     System.out.println("Hello India");
+//   }
+// }
+// class Test{
+//   public static synchronized void main(String args[]){
+//     System.out.println("Hello world");
+//   }
+// }
+// class Test{
+//   public static strictfp void main(String args[]){
+//     System.out.println("Hello alfaiz");
+//   }
+// }
+class Test{
+  public static final synchronized strictfp void main(String args[]){
+    System.out.println("Hello alfaiz");
+  }
+}
