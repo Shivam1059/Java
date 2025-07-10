c97f5f960428cb4faf5503727ca4d7381c6e6515
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-10 11:27:11 +0530

    add new

--- assignment_sheet_1/ass5.java
+++ assignment_sheet_1/ass5.java
@@ -2,7 +2,14 @@
 
 class Test{
   public static void main(String args[]){
-    
-    systme.out.println()
+    int total_cost = 1600;
+    int rate = 25;
+    int len = 20;
+    // parameter = 2*(l + b)
+    int park_para = total_cost/rate;
+    int p = park_para/2;
+    int bre = p - len ;
+
+    System.out.println("Parak parameter : "+park_para+"/n Parak breadth : "+bre);
   }
 }
