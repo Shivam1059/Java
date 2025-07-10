32fc50f07ee983d80436ecdeee33a657175c5682
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-10 10:58:37 +0530

    add new file

---
+++ assignment_sheet_1/ass44.java
@@ -0,0 +1,11 @@
+//ass44
+class Test{
+  public static void main(String args[]){
+    float n  = 28.0f;
+    int a = -21;
+    int d = 3;
+    float an = (float)(a + (n-1)*d);
+    System.out.println("The 28th term will be "+ an);
+    
+  }
+}
---
+++ assignment_sheet_1/ass45.java
@@ -0,0 +1,8 @@
+class Test{
+  public static void main(String args[]){
+    float n = 28 , sn ;
+    int a = -21, d = 3;
+    sn = (float)(n/2*(2*a+(n-1)*d));
+    System.out.printf("The sum upto 28th term will be %.0f ", sn);
+  }
+}
