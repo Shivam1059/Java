8f662c5b089882a2b6071c4f91f131ef4075e0cf
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-10 11:59:07 +0530

    add new file

--- assignment_sheet_1/ass14.java
+++ assignment_sheet_1/ass14.java
@@ -1,1 +1,11 @@
 //ass 14
+class Test{
+  public static void main(String args[]){
+    // formula = 1/2*d*(h1+h2);
+    int d = 46;
+    int h1 = 13;
+    int h2 = 10;
+    int area = (d*(h1+h2))/2;
+    System.out.println(area);
+  }
+}
--- assignment_sheet_1/ass33.java
+++ assignment_sheet_1/ass33.java
@@ -5,10 +5,13 @@ class Test{
     int garden_bre = 20;
     int path_len = 3;
     int path_bre = 4;
-    int garden_len_main = garden_len-path_len;
-    int garden_bre_main = garden_bre-path_bre;
+    int garden_len_main = garden_len-path_len;//27
 
-    int garden_area = garden_len_main*garden_bre_main;
+    int garden_bre_main = garden_bre-path_bre;
+    float  one_garden_len = garden_len_main/2.0f;
+    float one_garden_bre = garden_bre_main/2.0f;
+
+    int garden_area = (int)(one_garden_len*one_garden_bre);
     int total_garden_area = 4*garden_area;
     System.out.println(total_garden_area);
   }
