890871b1ea08ee1a45681a534ddd2df696c1a03c
Shivam Ahirwar <shivamahirwar0917109@gmail.com>
2025-07-11 09:36:38 +0530

    add new file

--- Assigments/assignment_1/ass34.java
+++ Assigments/assignment_1/ass34.java
@@ -1,6 +1,11 @@
 //ass 34
-class Test{
+class Trapezoid{
   public static void main(String args[]){
-    
+    int a = 128, b = 92, h = 40, wide = 4, widearea;
+    float woodearea = 1/2f*( a + b)*h;
+    widearea = wide*h;
+    int total_area =(int) woodearea + widearea;
+    System.out.println("Wooden area : "+woodearea+"wokeway area : "+ widearea+"after adding both area : "+total_area);
+
   }
 }
